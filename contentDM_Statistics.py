0c3b2f6ffbde8129359efcc32bcaa85882c9ee37
Jacob Grubb <jgrubb013@gmail.com>
2019-01-28 12:54:21 -0600

    Update contentDM_Statistics.py

--- contentDM_Statistics.py
+++ contentDM_Statistics.py
@@ -3,3 +3,9 @@
 # Team: Digital Initiatives
 # Programmer: Jacob Grubb (jagrubb@siue.edu)
 # File:  contentDM_Statistics.py
+
+#Look for external file containing contentDM collection list
+
+#Attempt to connect to contentDM server
+
+#Get username, password for contentDM domain
