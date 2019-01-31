0e198ebeb2f50f962100ca120887549e92d0a21b
Jake-Grubb <jagrubb@siue.edu>
2019-01-31 15:05:31 -0600

    Removed dependency not currently working

--- contentDM_Statistics.py
+++ contentDM_Statistics.py
@@ -7,13 +7,15 @@
 #Dependencies
 import json
 import csv
-import requests
+#import requests
 
 collection_list = []
 #Look for external file containing contentDM collection list
 with open("./collection_list.json", 'r') as jsonReader:
   collection_list = json.load(jsonReader)
 
+for item in collection_list:
+	print item
 #Attempt to connect to contentDM server
 
 #Get username, password for contentDM domain
